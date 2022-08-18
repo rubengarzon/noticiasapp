65948ded353355d87eaaf8e00a159f8ae22a978a
Rubén Garzón <rubengarzonzafra@gmail.com>
2022-08-18 12:34:53 +0200

    pagina de encabezados

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -3,10 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  public categories: string[] = [
+    'business',
+    'entertainment',
+    'general',
+    'health',
+    'science',
+    'sports',
+    'technology',
+  ];
+
+  public selectedCategory: string = this.categories[0];
 
   constructor() {}
 
+  segmentChanged(category: any) {
+    console.log(category);
+  }
 }
